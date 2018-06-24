fe1c76bd1fe095bc12eb3366a14ac95a9e3ee618
Meliphant <op.personal.analytics@gmail.com>
2018-06-24 13:04:31 +0300

    added InventoryContract class

---
+++ app/src/main/java/com/example/android/inventoryapp_udacity_project/data/InventoryContract.java
@@ -0,0 +1,32 @@
+package com.example.android.inventoryapp_udacity_project.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class InventoryContract {
+
+        private InventoryContract() {}
+
+        public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp_udacity_project";
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+        public static final String PATH_INVENTORY = "inventoryapp_udacity_project";
+
+        public static final class InventoryrEntry implements BaseColumns {
+
+            public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+            public static final String CONTENT_LIST_TYPE =
+                    ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+            public static final String CONTENT_ITEM_TYPE =
+                    ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+            public final static String TABLE_NAME = "inventory";
+            public final static String _ID = BaseColumns._ID;
+            public final static String COLUMN_PRODUCT_NAME ="product_name";
+            public final static String COLUMN_PRODUCT_PRICE = "price";
+            public final static String COLUMN_QUANTITY = "quantity";
+            public final static String COLUMN_SUPPLIER_NAME = "supplier_name";
+            public final static String COLUMN_SUPPLIER_PHONE = "supplier_phone";
+
+        }
+}

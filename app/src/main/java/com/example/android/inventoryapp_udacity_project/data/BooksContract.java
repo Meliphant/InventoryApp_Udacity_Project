42e507daffd04900d13730ccb8f2a9bb61438694
Meliphant <op.personal.analytics@gmail.com>
2018-06-24 13:14:32 +0300

    added InventorydDbHelper class

--- app/src/main/java/com/example/android/inventoryapp_udacity_project/data/BooksContract.java
+++ app/src/main/java/com/example/android/inventoryapp_udacity_project/data/BooksContract.java
@@ -4,15 +4,15 @@ import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-public class InventoryContract {
+public class BooksContract {
 
-        private InventoryContract() {}
+        private BooksContract() {}
 
         public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp_udacity_project";
         public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
         public static final String PATH_INVENTORY = "inventoryapp_udacity_project";
 
-        public static final class InventoryrEntry implements BaseColumns {
+        public static final class BooksEntry implements BaseColumns {
 
             public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
             public static final String CONTENT_LIST_TYPE =
@@ -20,7 +20,7 @@ public class InventoryContract {
             public static final String CONTENT_ITEM_TYPE =
                     ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
 
-            public final static String TABLE_NAME = "inventory";
+            public final static String TABLE_NAME = "books";
             public final static String _ID = BaseColumns._ID;
             public final static String COLUMN_PRODUCT_NAME ="product_name";
             public final static String COLUMN_PRODUCT_PRICE = "price";
---
+++ app/src/main/java/com/example/android/inventoryapp_udacity_project/data/InventoryDbHelper.java
@@ -0,0 +1,33 @@
+package com.example.android.inventoryapp_udacity_project.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_BOOKS_TABLE =  "CREATE TABLE " + BooksContract.BooksEntry.TABLE_NAME + " ("
+                + BooksContract.BooksEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BooksContract.BooksEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + BooksContract.BooksEntry.COLUMN_PRODUCT_PRICE + " TEXT NOT NULL DEFAULT 0, "
+                + BooksContract.BooksEntry.COLUMN_QUANTITY + " INTEGER NOT NULL, "
+                + BooksContract.BooksEntry.COLUMN_SUPPLIER_NAME + " INTEGER, "
+                + BooksContract.BooksEntry.COLUMN_SUPPLIER_PHONE + " INTEGER);";
+
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
